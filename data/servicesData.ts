e6e0de4bc2bc982bb7ef334ea7b7f3227e4169bb
kelvinguchu <kelvinguchu5@gmail.com>
2024-10-03 19:59:00 +0300

    services data creation

---
+++ data/servicesData.ts
@@ -0,0 +1,74 @@
+export const services = [
+  {
+    iconSrc: "/icons/flowchart.svg",
+    title: "Digital PR & Content",
+    description:
+      "We streamline your business processes with innovative implementation of digital strategies. Stay ahead with automation and data-driven insights.",
+  },
+  {
+    iconSrc: "/icons/rocket.svg",
+    title: "Branding and Brand Development",
+    description:
+      "We help you maintain a unified voice across channels, reinforce your brand identity and resonate with your audience with intelligent creative expression.",
+  },
+  {
+    iconSrc: "/icons/flag.svg",
+    title: "Social Media Management & Advertising",
+    description:
+      "We create and manage targeted advertising campaigns on popular social media platforms such as Facebook, Instagram, Twitter, and LinkedIn.",
+  },
+  {
+    iconSrc: "/icons/line-up.svg",
+    title: "Search Engine Marketing",
+    description:
+      "Our SEM services include pay-per-click (PPC) advertising on search engines like Google and Bing, as well as search engine optimization.",
+  },
+  {
+    iconSrc: "/icons/mail-open.svg",
+    title: "Email Marketing",
+    description:
+      "Our email marketing services involve crafting compelling email campaigns to nurture leads and engage existing customers.",
+  },
+  {
+    iconSrc: "/icons/certificate.svg",
+    title: "Display Advertising",
+    description:
+      "We create eye-catching display ads that capture attention and drive engagement across various online platforms and websites.",
+  },
+  {
+    iconSrc: "/icons/video-play.svg",
+    title: "Digital Content Creation",
+    description:
+      "We produce high-quality media content that matches your distinct brand style and is customized to your business objectives at scale.",
+  },
+  {
+    iconSrc: "/icons/media.svg",
+    title: "Media Planning & Research",
+    description:
+      "We analyze market trends and audience insights to create effective media strategies, ensuring your content reaches the right audience.",
+  },
+  {
+    iconSrc: "/icons/set.svg",
+    title: "Data Driven Audience Segmentation",
+    description:
+      "We utilize advanced analytics to segment your audience effectively, allowing for tailored marketing efforts that enhance engagement.",
+  },
+  {
+    iconSrc: "/icons/tool.svg",
+    title: "Leveraging ML and AI To Deliver Precision Campaigns",
+    description:
+      "We integrate machine learning and artificial intelligence to optimize your campaigns, ensuring precision targeting and improved performance.",
+  },
+  {
+    iconSrc: "/icons/bulb.svg",
+    title: "Optimized Media Plans Based On Campaign Objectives",
+    description:
+      "We develop media plans that align with your specific campaign goals, ensuring efficient use of resources and maximum impact.",
+  },
+  {
+    iconSrc: "/icons/growth.svg",
+    title: "Tracking And Measuring Campaign Performance",
+    description:
+      "We provide comprehensive analytics and reporting to track your campaign performance, allowing for informed adjustments and improvements.",
+  },
+];
